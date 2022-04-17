b44075d705d50e756e721187e7888dea4dfe1462
Phearak S. Tha <t.sophearak@gmail.com>
2022-04-17 22:11:37 +0700

    Add favicon and bump version

--- .moul/deploy.sh
+++ .moul/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -xe
-curl -LJO https://github.com/moulapp/moul/releases/download/v4.0.0-beta.11/moul_4.0.0-beta.11_Linux_x86_64.tar.gz
-tar -xzf moul_4.0.0-beta.11_Linux_x86_64.tar.gz -C ./
+curl -LJO https://github.com/moulapp/moul/releases/download/v4.0.0-beta.12/moul_4.0.0-beta.12_Linux_x86_64.tar.gz
+tar -xzf moul_4.0.0-beta.12_Linux_x86_64.tar.gz -C ./
 ./moul deploy
 cd ./.moul && npm i
 npm run build
