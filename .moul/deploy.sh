a9b61d7763801c49d5092a5ac99a07805da93f9f
Phearak S. Tha <t.sophearak@gmail.com>
2022-04-18 21:17:41 +0700

    Bump version

--- .moul/deploy.sh
+++ .moul/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -xe
-curl -LJO https://github.com/moulapp/moul/releases/download/v4.0.0-beta.12/moul_4.0.0-beta.12_Linux_x86_64.tar.gz
-tar -xzf moul_4.0.0-beta.12_Linux_x86_64.tar.gz -C ./
+curl -LJO https://github.com/moulapp/moul/releases/download/v4.0.0-beta.13/moul_4.0.0-beta.13_Linux_x86_64.tar.gz
+tar -xzf moul_4.0.0-beta.13_Linux_x86_64.tar.gz -C ./
 ./moul deploy
-cd ./.moul && npm i
-npm run build
+npm install && npm run build
