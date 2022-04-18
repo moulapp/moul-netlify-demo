92787b4a1a4c1c045da1212e9f3a480eb578d946
Phearak S. Tha <t.sophearak@gmail.com>
2022-04-18 22:37:12 +0700

    Bump env

--- .moul/deploy.sh
+++ .moul/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -xe
-curl -LJO https://github.com/moulapp/moul/releases/download/v4.0.0-beta.13/moul_4.0.0-beta.13_Linux_x86_64.tar.gz
-tar -xzf moul_4.0.0-beta.13_Linux_x86_64.tar.gz -C ./
+curl -LJO https://github.com/moulapp/moul/releases/download/v4.0.0-beta.14/moul_4.0.0-beta.14_Linux_x86_64.tar.gz
+tar -xzf moul_4.0.0-beta.14_Linux_x86_64.tar.gz -C ./
 ./moul deploy
 npm install && npm run build
